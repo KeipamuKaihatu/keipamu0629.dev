c2ea2480a0669dfaf672d1243a1368e6fca350cf
KeipamuJP <keipamu0629@outlook.jp>
2025-07-22 13:37:25 +0900

    a

--- script/index.js
+++ script/index.js
@@ -1,14 +1,14 @@
-  const toggle = document.getElementById('menuToggle');
-  const nav = document.getElementById('navLinks');
+const toggle = document.getElementById('menuToggle');
+const nav = document.getElementById('navLinks');
 
-  toggle.addEventListener('click', () => {
-    nav.classList.toggle('open');
-  });
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
 
-  // メニュー外クリックで自動的に閉じる処理
-  document.addEventListener('click', (e) => {
-    const isClickInside = toggle.contains(e.target) || nav.contains(e.target);
-    if (!isClickInside) {
-      nav.classList.remove('open');
-    }
-  });
+// メニュー外クリックで自動的に閉じる処理
+document.addEventListener('click', (e) => {
+  const isClickInside = toggle.contains(e.target) || nav.contains(e.target);
+  if (!isClickInside) {
+    nav.classList.remove('open');
+  }
+});
